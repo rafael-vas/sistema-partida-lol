3d2c815d041631b175422ba9bb5fdb42461cb38b
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2026-04-07 17:41:34 -0300

    feat: cria estrutura inicial do simulador de partida LoL

---
+++ src/sistema/app/Main.java
@@ -0,0 +1,18 @@
+package sistema.app;
+
+import sistema.model.Assassino;
+import sistema.model.Atirador;
+import sistema.model.Item;
+import sistema.model.Lutador;
+import sistema.model.Mago;
+import sistema.model.Campeao;
+import sistema.model.Suporte;
+import sistema.model.Tanque;
+import sistema.model.Time;
+import sistema.service.Partida;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Criando a main");
+    }
+}
