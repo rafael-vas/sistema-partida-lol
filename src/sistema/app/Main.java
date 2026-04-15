41a880860c461d0eb92db3a6e065de92e18603a4
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2026-04-15 14:16:17 -0300

    atualizar main no contexto do novo projeto

--- src/sistema/app/Main.java
+++ src/sistema/app/Main.java
@@ -1,18 +1,7 @@
 package sistema.app;
 
-import sistema.model.Assassino;
-import sistema.model.Atirador;
-import sistema.model.Item;
-import sistema.model.Lutador;
-import sistema.model.Mago;
-import sistema.model.Campeao;
-import sistema.model.Suporte;
-import sistema.model.Tanque;
-import sistema.model.Time;
-import sistema.service.Partida;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Criando a main");
+        System.out.println("Main do QA Track");
     }
 }
